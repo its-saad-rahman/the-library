4ac26022b301e6803623366a8002a4effb32aa50
Saad Rahman <itssaadrahman@gmail.com>
2022-08-17 22:51:40 +0600

    Added Book function

---
+++ main.js
@@ -0,0 +1,11 @@
+let myLibrary = [];
+
+function Book(title, author, pages, genre, publishedYear) {
+  title;
+  author;
+  pages;
+  genre;
+  publishedYear;
+}
+
+Book.prototype.addBookToLibrary = function () {};
