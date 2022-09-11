c18c0d59d76325df3f43254e3f742937c1fc6622
Saad Rahman <itssaadrahman@gmail.com>
2022-09-11 12:13:58 +0600

    Added variables

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 let myLibrary = [];
 
 function Book(title, author, pages, genre, publishedYear) {
-  title;
-  author;
-  pages;
-  genre;
-  publishedYear;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.genre = genre;
+  this.publishedYear = publishedYear;
 }
 
-Book.prototype.addBookToLibrary = function () {};
+const title = document.querySelector('[data-title]');
+const author = document.querySelector('[data-author]');
+const page = document.querySelector('[data-page]');
+const year = document.querySelector('[data-year]');
+const genre = document.querySelector('[data-genre]');
+const addBookBtn = document.querySelector('.add-book--btn');
+
+Book.prototype.addBookToLibrary = function (e) {
+  myLibrary.push(e);
+};
+Book.prototype.removeBookFromLibrary = function () {};
+Book.prototype.displayBook = function () {};
+Book.prototype.readingBook = function () {};
+
+const book = new Book('The Hobbit', 'J.R.R Tolkin', '376', '1945', 'Fantasy');
+
+addBookBtn.addEventListener('click', () => {
+  book.addBookToLibrary(e);
+});
